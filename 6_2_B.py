36e9ca6e46caf77932fc8eb7f3d11ed3db40783d
mgranot1 <80787986+mgranot1@users.noreply.github.com>
2022-05-10 16:57:35 +0300

    try_2

--- 6_2_B.py
+++ 6_2_B.py
@@ -1,15 +1,15 @@
-def get_positive_numbers():
+def get_positive_numbers(original_str:str):
     """
-    Receiving input of numbers from the user
+    :param original_str: Input of numbers from the user
     :return: List of positive numbers in type int
     """
-    original_str = input("Enter numbers separated by commas: ")
     list_of_input_numbers = original_str.split(",")
     return list(filter(lambda x: x > 0, list(map(lambda x: int(x), list_of_input_numbers))))
 
 
 def main():
-    print(get_positive_numbers())
+    original_str = input("Enter numbers separated by commas: ")
+    print(get_positive_numbers(original_str))
 
 
 if __name__ == "__main__":
